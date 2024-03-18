4ffaa307fe32e5bd19a08bb54093acdbbff2b967
Ankush Yadav <138442615+Mswpresents@users.noreply.github.com>
2024-03-18 10:53:29 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -55,7 +55,7 @@ async def pub_(bot, message):
        return await stop(client, user)
     temp.forwardings += 1
     await db.add_frwd(user)
-    await send(client, user, "<b>🧡 ғᴏʀᴡᴀʀᴅɪɴɢ sᴛᴀʀᴛᴇᴅ 🥀 <a href=https://t.me/Jisshu_support>SUPPORT</a>🥀</b>")
+    await send(client, user, "<b>🧡 ғᴏʀᴡᴀʀᴅɪɴɢ sᴛᴀʀᴛᴇᴅ 🥀 <a href=https://t.me/mswpresent>UPDATES</a>🥀</b>")
     sts.add(time=True)
     sleep = 1 if _bot['is_bot'] else 10
     await msg_edit(m, "<code>Processing...</code>") 
@@ -109,7 +109,7 @@ async def pub_(bot, message):
             temp.IS_FRWD_CHAT.remove(sts.TO)
             return await stop(client, user)
         temp.IS_FRWD_CHAT.remove(sts.TO)
-        await send(client, user, "<b>🎉 ғᴏʀᴡᴀᴅɪɴɢ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 🥀 <a href=https://t.me/Jisshu_support>SUPPORT</a>🥀</b>")
+        await send(client, user, "<b>🎉 ғᴏʀᴡᴀᴅɪɴɢ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 🥀 <a href=praveenthakur07@axl>Donate Us</a>🥀</b>")
         await edit(m, 'Completed', "completed", sts) 
         await stop(client, user)
             
@@ -197,8 +197,8 @@ async def edit(msg, title, status, sts):
    text = TEXT.format(i.fetched, i.total_files, i.duplicate, i.deleted, i.skip, status, percentage, estimated_total_time, progress)
    if status in ["cancelled", "completed"]:
       button.append(
-         [InlineKeyboardButton('Support', url='https://t.me/Jisshu_support'),
-         InlineKeyboardButton('Updates', url='https://t.me/JISSHU_BOTS')]
+         [InlineKeyboardButton('Support', url='https://t.me/TeamjaishriramBot'),
+         InlineKeyboardButton('Updates', url='https://t.me/mswpresent')]
          )
    else:
       button.append([InlineKeyboardButton('• ᴄᴀɴᴄᴇʟ', 'terminate_frwd')])
